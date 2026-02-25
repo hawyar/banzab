280d9f169b3cf4c8e58a5b06f222ce4020c71f11
hawyar <hawyarfa@gmail.com>
2026-02-25 21:29:32 +0300

    Update favicon again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
